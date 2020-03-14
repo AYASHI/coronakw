a83142cbb94ae1c0a99e63c44cfab0b8db689511
Ahmad Ayashi <me@ahmadayashi.com>
2020-03-14 17:17:10 +0300

    create home screen

---
+++ src/features/home/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+export default HomeScreen;
