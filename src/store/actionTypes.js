762c5039c089a3f4702e67cef3092a9aa2dd3d3c
Ahmad Ayashi <me@ahmadayashi.com>
2020-03-14 17:17:40 +0300

    action types

---
+++ src/store/actionTypes.js
@@ -0,0 +1,9 @@
+// Login action
+export const TRY_LOGIN = 'TRY_LOGIN';
+export const TRY_LOGIN_SUCCESS = 'TRY_LOGIN_SUCCESS';
+export const TRY_LOGIN_FAILED = 'TRY_LOGIN_FAILED';
+
+// register action
+export const TRY_REGISTER = 'TRY_REGISTER';
+export const TRY_REGISTER_SUCCESS = 'TRY_REGISTER_SUCCESS';
+export const TRY_REGISTER_FAILED = 'TRY_REGISTER_FAILED';
