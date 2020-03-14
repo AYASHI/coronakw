5231eca340e1e73a8e166748124a2b62a62ff077
Ahmad Ayashi <me@ahmadayashi.com>
2020-03-14 17:16:51 +0300

    add auth screens, reducer and saga

---
+++ src/features/auth/RegistrationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RegistrationScreen = () => {
+  return (
+    <View>
+      <Text>Registration Screen</Text>
+    </View>
+  );
+};
+
+export default RegistrationScreen;
