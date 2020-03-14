444b3c32235efb045e028fabb0962a0e35aaec95
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-14 23:14:31 +0300

    login screen

--- src/features/auth/LoginScreen.js
+++ src/features/auth/LoginScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
+import images from '../../utils/images';
+import Button from '../../components/Button';
+import Spacer from '../../components/Spacer';
 
-const LoginScreen = () => {
+const LoginScreen = ({navigation}) => {
+  const login = () => {
+    navigation.navigate('Home');
+  };
   return (
-    <View>
-      <Text> LoginScreen</Text>
+    <View style={styles.container}>
+      <Image source={images.logo} />
+      <Spacer />
+      <Button text={'تسجيل الدخول'} onPress={login} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default LoginScreen;
