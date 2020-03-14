0d14d98524f6a8cc58462335876268a75125c328
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-14 23:13:21 +0300

    home screen

--- src/features/home/HomeScreen.js
+++ src/features/home/HomeScreen.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import fonts from '../../utils/fonts';
+import HomeScreenHeader from '../../components/HomeScreenHeader';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={{fontFamily: fonts.Bold}}>رعاية تك</Text>
+      <HomeScreenHeader />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 export default HomeScreen;
