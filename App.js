af32964c7ad4a0c493df9096253c63e75ba13137
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-14 23:10:51 +0300

    install and configure reactnavigation as a navigation framwork

--- App.js
+++ App.js
@@ -7,14 +7,14 @@
  */
 
 import React from 'react';
-import HomeScreen from './src/features/home/HomeScreen';
 import {Provider} from 'react-redux';
 import store from './src/store/store';
+import AppNavigator from './src/navigators/AppNavigator';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <AppNavigator />
     </Provider>
   );
 };
