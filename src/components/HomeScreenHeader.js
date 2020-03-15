1a830ddd92e746fce46c140703253a1cf2daccfc
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-15 05:38:24 +0300

    update home screen header

--- src/components/HomeScreenHeader.js
+++ src/components/HomeScreenHeader.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import colors from '../utils/colors';
+import fonts from '../utils/fonts';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import layout from '../utils/layout';
+import StatusSelectionView from './StatusSelectionView';
 
 const HomeScreenHeader = () => {
   return (
-    <View style={styles.container}>
-      <Text> </Text>
-    </View>
+    <Fragment>
+      <SafeAreaView style={styles.saveArea} />
+      <View style={styles.container}>
+        <Text style={styles.lastUpdateText}>اخر تحديث منذ ساعة</Text>
+        <Text style={styles.howDoYouFeel}>كيف تشعر الأن؟</Text>
+        <StatusSelectionView />
+      </View>
+    </Fragment>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    height: 218,
-    width: '100%',
+  saveArea: {
     backgroundColor: colors.status.green,
+  },
+  container: {
+    height: 174,
+    width: '100%',
+    padding: layout.margin,
+    paddingTop: 22,
+    backgroundColor: colors.status.green,
+  },
+  lastUpdateText: {
+    color: colors.opacityWhite,
+    fontFamily: fonts.Semibold,
+    fontSize: 15,
+    textAlign: 'left',
+  },
+  howDoYouFeel: {
+    fontFamily: fonts.Bold,
+    textAlign: 'left',
+    color: 'white',
+    fontSize: 32,
   },
 });
 
