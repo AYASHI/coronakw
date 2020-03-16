6e52ec72370f06d625b2c360daef9bb412aeee0f
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-16 18:50:48 +0300

    new colors add

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,8 +1,10 @@
 const colors = {
   green: '#009641',
   opacityWhite: 'rgba(255,255,255,0.6)',
+  opacityBlack: 'rgba(0, 0, 0, 0.6)',
   separatorColor: '#eaeef1',
   paleGrey: '#f5f5f9',
+  gray: '#666666',
   status: {
     green: '#02b875',
     yellow: '#c8910d',
