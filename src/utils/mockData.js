e2b46cfe44d6f1d68814484d3e3137b0cf3ec34e
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-16 18:50:38 +0300

    move mockdata to seperate file

---
+++ src/utils/mockData.js
@@ -0,0 +1,14 @@
+export const questions = [
+  {
+    id: '1',
+    questionText: 'ارتفاع درجة الحرارة الى ما يشبه الحمى',
+  },
+  {id: '2', questionText: 'السعال الجاف والكحة المؤلمة'},
+  {id: '3', questionText: 'سيلان الأنف بإستمرار'},
+  {id: '4', questionText: 'إسهال'},
+  {
+    id: '5',
+    questionText: 'الشعور بالتعب وارهاق عام في الجسم',
+  },
+  {id: '6', questionText: 'الصعوبة بالتنفس'},
+];
