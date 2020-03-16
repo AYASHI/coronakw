ed82ef3e319e69999540b6046cf423ef827b6a23
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-16 18:51:02 +0300

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "https://29e02c26-6207-48f7-9f9e-5f98254acd64.mock.pstmn.io";
+export const BASE_URL =
+  'https://29e02c26-6207-48f7-9f9e-5f98254acd64.mock.pstmn.io';
 
 //health status
 export const HEALTHY = 1;
 export const UNHEALTHY = 2;
 export const SERIOUSLY_UNHEALTHY = 3;
-
