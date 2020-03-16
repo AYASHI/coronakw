280b435c91f53927b0451316020821270dd1331e
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-16 19:18:59 +0300

    enter temperature fragment styleing

--- src/components/EnterTemperatureFragment.js
+++ src/components/EnterTemperatureFragment.js
@@ -1,72 +1,84 @@
-import React, { Fragment, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Platform } from 'react-native';
+import React, {Fragment} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import layout from '../utils/layout';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as actionTypes from '../store/actionTypes';
 
-import Geolocation from 'react-native-geolocation-service';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import images from '../utils/images';
+import colors from '../utils/colors';
+import fonts from '../utils/fonts';
 
+const EnterTemperatureFragment = props => {
+  const temperatureButtonTouched = () => {
+    props.temperatureModalShown(true);
+  };
 
-const EnterTemperatureFragment = (props) => {
-
-
-    const temperatureButtonTouched = () => {
-        props.temperatureModalShown(true);
-    };
-
-    return (
-        <Fragment>
-            <View style={styles.container}>
-                <Text style={styles.subtitle}>
-                    كيف يمكننا مساعدتك
-                </Text>
-                <Button
-                    title="إدخل قياس درجة الحرارة"
-                    onPress={temperatureButtonTouched}
-                />
-            </View>
-        </Fragment>
-    );
+  const TemperatureView = () => (
+    <TouchableOpacity
+      style={styles.temperature}
+      onPress={temperatureButtonTouched}>
+      <Image source={images.temperature} />
+      <Text style={styles.subtitle}>إدخل قياس درجة الحرارة</Text>
+    </TouchableOpacity>
+  );
+  return (
+    <Fragment>
+      <View style={styles.container}>
+        <Text style={styles.title}>كيف يمكننا مساعدتك</Text>
+        <TemperatureView />
+      </View>
+    </Fragment>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        padding: layout.margin,
-    },
+  temperature: {
+    alignItems: 'center',
+    marginTop: 16,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 8,
+  },
+  container: {
+    width: '100%',
+    padding: layout.margin,
+  },
 
-    subtitle: {
-        color: 'rgba(60, 60, 67, 0.6)',
-        fontSize: 17,
-        textAlign: 'left',
-        padding: 5
-    },
-    title: {
-        // fontFamily: fonts.Bold,
-        textAlign: 'left',
-        // color: 'white',
-        fontSize: 20,
-        padding: 5
-    },
+  title: {
+    color: colors.gray,
+    fontSize: 17,
+    textAlign: 'left',
+    padding: 5,
+  },
+  subtitle: {
+    fontFamily: fonts.Medium,
+    textAlign: 'left',
+    fontSize: 17,
+    marginStart: 10,
+  },
 });
 
 // Map State To Props (Redux Store Passes State To Component)
-const mapStateToProps = (state) => {
-    // Redux Store --> Component
-    return {
-
-    };
+const mapStateToProps = state => {
+  // Redux Store --> Component
+  return {};
 };
 
 // Map Dispatch To Props (Dispatch Actions To Reducers. Reducers Then Modify The Data And Assign It To Your Props)
-const mapDispatchToProps = (dispatch) => {
-    // Action
-    return {
-        temperatureModalShown: (isShown) => dispatch({
-            type: actionTypes.TEMPERATURE_MODAL_SHOWN,
-            value: isShown,
-        })
-    };
+const mapDispatchToProps = dispatch => {
+  // Action
+  return {
+    temperatureModalShown: isShown =>
+      dispatch({
+        type: actionTypes.TEMPERATURE_MODAL_SHOWN,
+        value: isShown,
+      }),
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(EnterTemperatureFragment);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(EnterTemperatureFragment);
