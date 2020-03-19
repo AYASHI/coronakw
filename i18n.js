b7f7113be05e126aa8888f746c2cdb61c81725ee
Bader Alsayegh <bader.a@hypertext.com.kw>
2020-03-19 20:34:35 +0300

    added language detector to i18n.js

--- i18n.js
+++ i18n.js
@@ -3,6 +3,13 @@ import {initReactI18next} from 'react-i18next';
 // import Backend from 'i18next-xhr-backend';
 import en from './src/locales/en-translations.json';
 import ar from './src/locales/ar-translations.json';
+const languageDetector = {
+  type: 'languageDetector',
+  async: true,
+  detect: cb => cb('en'),
+  init: () => {},
+  cacheUserLanguage: () => {},
+};
 
 i18n
   // load translation using xhr -> see /public/locales
@@ -11,11 +18,12 @@ i18n
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   // pass the i18n instance to react-i18next.
+  .use(languageDetector)
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'ar',
+    fallbackLng: 'en',
     debug: true,
     resources: {
       en: {
