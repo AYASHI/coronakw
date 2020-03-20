9a430d5fea48edb9683100a7e41adc2ea9d3af23
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-03-21 00:34:26 +0300

    update spacer component

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -3,7 +3,9 @@ import {View} from 'react-native';
 import layout from '../utils/layout';
 
 const Spacer = props => {
-  return <View style={{height: props.space ? props.space : layout.margin}} />;
+  const space = props.space ? props.space : layout.margin;
+  const style = props.horizontal ? {width: space} : {height: space};
+  return <View style={style} />;
 };
 
 export default Spacer;
