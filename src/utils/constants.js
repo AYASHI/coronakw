e555c59c4d9acf213667c5c69dc5ecfab96f9b6b
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-04-03 21:34:47 +0300

    new api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,10 @@ import {I18nManager} from 'react-native';
 export const MOCK_BASE_URL =
   'https://3e73db34-34c9-4fae-9ea3-5d501573e04e.mock.pstmn.io';
 const language = I18nManager.isRTL ? 'ar' : 'en';
-
+// https://test-shlonikapi.azurefd.net
+// https://test-shelonekapi.azurewebsites.net/
 export const BASE_URL =
-  'https://test-shelonekapi.azurewebsites.net/' + language + '/api/v1';
+  'https://test-shlonikapi.azurefd.net/' + language + '/api/v1';
 
 //health status
 export const HEALTHY = 1;
