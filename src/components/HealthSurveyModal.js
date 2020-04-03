3525b3e553e30362a13ff9eba34663f093a9ffb0
Ahmad Ayashi <ayashi.cs@gmail.com>
2020-04-03 13:39:21 +0300

    for single selection button text only 'confirm'

--- src/components/HealthSurveyModal.js
+++ src/components/HealthSurveyModal.js
@@ -117,8 +117,9 @@ const HealthSurveyModal = props => {
       : 0;
 
   var buttonMsg = t('healthSurveyModal.confirm') + ` ${numberOfSymptoms} ` + t('healthSurveyModal.symptoms') ;
-
-  if (numberOfSymptoms === 0) {
+  if (props.single) {
+     buttonMsg = t('healthSurveyModal.confirm')
+  } else if (numberOfSymptoms === 0) {
     buttonMsg = t('healthSurveyModal.noSymptoms');
   } else if (numberOfSymptoms === 1) {
     buttonMsg = t('healthSurveyModal.oneSymptom');
